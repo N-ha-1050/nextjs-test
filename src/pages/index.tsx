e6a10fc8ab3a8c8cc28c5d55ef279f868f98cbf9
N_ha <naganoharuho@gmail.com>
2022-01-22 17:54:01 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      <Text>Hello, world!</Text>
+    </Box>
+  )
+}
+
+export default Home
